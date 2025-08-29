d09bbb61b86056af8cd2db86bdd20d1dd121d659
changyunyeong <sally0109277@naver.com>
2025-08-29 13:35:35 +0900

    feat: oauth 설정 #3

---
+++ server/src/main/java/com/growplan/global/oauth/userinfo/KakaoUserInfo.java
@@ -0,0 +1,45 @@
+package com.growplan.global.oauth.userinfo;
+
+import lombok.AllArgsConstructor;
+
+import java.util.Map;
+
+import lombok.AllArgsConstructor;
+
+import java.util.Map;
+
+@AllArgsConstructor
+public class KakaoUserInfo implements OAuth2UserInfo {
+
+    private Map<String, Object> attributes;
+
+    @Override
+    public String getProviderId() {
+        return attributes.get("id").toString();
+    }
+
+    @Override
+    public String getProvider() {
+        return "kakao";
+    }
+
+    public String getEmail() {
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        if (kakaoAccount == null) {
+            return null;
+        }
+        return (String) kakaoAccount.get("email");
+    }
+
+    public String getName() {
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        if (kakaoAccount == null) {
+            return null;
+        }
+        Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
+        if (profile == null) {
+            return null;
+        }
+        return (String) profile.get("nickname");
+    }
+}

505c51e8b2b62c85f9bb2fc64f64d4c4e48a219a
changyunyeong <sally0109277@naver.com>
2025-08-30 17:27:36 +0900

    feat: enum 형식 통일 #14

--- server/src/main/java/com/growplan/global/common/enums/InvestmentPurpose.java
+++ server/src/main/java/com/growplan/global/common/enums/InvestmentPurpose.java
@@ -7,13 +7,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum InvestmentPurpose { // 투자 목적
 
-    SAVINGS("저축"),
-    HOME_OWNERSHIP("내 집 마련"),
-    CAR_PURCHASE("차량 구매"),
-    TRAVEL("여행"),
-    MARRIAGE("결혼"),
-    SELF_DEVELOPMENT("자기계발"),
-    OTHER("기타");
-
-    private final String purpose;
+    SAVINGS,           // 저축
+    HOME_OWNERSHIP,    // 내 집 마련
+    CAR_PURCHASE,      // 차량 구매
+    TRAVEL,            // 여행
+    MARRIAGE,          // 결혼
+    SELF_DEVELOPMENT,  // 자기계발
+    OTHER              // 기타
 }

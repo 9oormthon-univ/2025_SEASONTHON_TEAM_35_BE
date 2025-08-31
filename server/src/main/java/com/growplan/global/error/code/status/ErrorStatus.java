725efc4ccb0c15f68999f5a7c104cc46ead81568
changyunyeong <sally0109277@naver.com>
2025-08-31 14:57:49 +0900

    feat: 자산 정보가 없을 시 예외 추가 #16

--- server/src/main/java/com/growplan/global/error/code/status/ErrorStatus.java
+++ server/src/main/java/com/growplan/global/error/code/status/ErrorStatus.java
@@ -20,6 +20,9 @@ public enum ErrorStatus implements BaseErrorCode {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER404", "멤버를 찾을 수 없습니다."),
     MEMBER_EXIST(HttpStatus.CONFLICT, "MEMBER409", "멤버가 이미 존재합니다."),
 
+    // Asset
+    ASSET_NOT_FOUND(HttpStatus.NOT_FOUND, "ASSET404", "아직 자산을 입력하지 않았습니다."),
+
     // Jwt
     EMPTY_TOKEN(HttpStatus.UNAUTHORIZED, "TOKEN400", "헤더에 토큰이 비어 있습니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "TOKEN401", "토큰이 유효하지 않습니다."),

bcfa8a50b2bc340b5eca8c17994065077a1498b6
changyunyeong <sally0109277@naver.com>
2025-09-01 17:04:00 +0900

    feat: 자산 수정 기능 추가 #16

--- server/src/main/java/com/growplan/domain/asset/service/AssetQueryService.java
+++ server/src/main/java/com/growplan/domain/asset/service/AssetQueryService.java
@@ -1,9 +1,13 @@
 package com.growplan.domain.asset.service;
 
+import com.growplan.domain.asset.dto.AssetRequestDTO;
 import com.growplan.domain.asset.dto.AssetResponseDTO;
 import com.growplan.domain.member.entity.Member;
 
 public interface AssetQueryService {
 
     AssetResponseDTO.RegisterAssetResponseDTO getAssetSummary(Member member);
+    void updateInvestment(Member member, AssetRequestDTO.UpdateInvestmentsRequest request);
+    void updateCash(Member member, AssetRequestDTO.UpdateCashRequest request);
+    void updateOthers(Member member, AssetRequestDTO.UpdateOthersRequest request);
 }

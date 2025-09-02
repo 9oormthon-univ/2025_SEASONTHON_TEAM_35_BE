b1c8ae3ed23f5d894035a12c205a88300d426539
changyunyeong <sally0109277@naver.com>
2025-09-02 18:38:24 +0900

    hotfix: enum 수정

--- server/src/main/java/com/growplan/global/common/enums/ProfitRange.java
+++ server/src/main/java/com/growplan/global/common/enums/ProfitRange.java
@@ -8,8 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum ProfitRange { // 투자 수익에 대한 목표 금액
 
     BELOW_500,       // 500만원 이하
-    BETWEEN_500_1500, // 500-1500만원
-    BETWEEN_1500_3000,// 1500-3000만원
+    BETWEEN_500_1000, // 500-1000만원
+    BETWEEN_1000_3000,// 1000-3000만원
     BETWEEN_3000_5000,// 3000-5000만원
     BETWEEN_5000_10000,// 5000-1억
     ABOVE_10000      // 1억 이상

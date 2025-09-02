80493ea83f55466e64d26053e66ad754d926e9bc
changyunyeong <sally0109277@naver.com>
2025-09-02 17:56:02 +0900

    feat: 마이페이지 구현 #21

--- server/src/main/java/com/growplan/domain/member/dto/MemberResponseDTO.java
+++ server/src/main/java/com/growplan/domain/member/dto/MemberResponseDTO.java
@@ -1,9 +1,12 @@
 package com.growplan.domain.member.dto;
 
+import com.growplan.domain.asset.entity.AssetPortfolio;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 public class MemberResponseDTO {
 
@@ -13,5 +16,15 @@ public class MemberResponseDTO {
     @AllArgsConstructor
     public static class LoginSuccessDTO {
         String accessToken;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MyPageDTO {
+        String name;
+        String email;
+        BigDecimal totalAmount;
     }
 }
--- server/src/main/java/com/growplan/domain/member/service/MemberService.java
+++ server/src/main/java/com/growplan/domain/member/service/MemberService.java
@@ -1,9 +1,11 @@
 package com.growplan.domain.member.service;
 
+import com.growplan.domain.member.dto.MemberResponseDTO;
 import com.growplan.domain.member.entity.Member;
 import jakarta.servlet.http.HttpServletResponse;
 
 public interface MemberService {
 
     void logout(Member member, HttpServletResponse response);
+    MemberResponseDTO.MyPageDTO getMyPage(Member member);
 }
